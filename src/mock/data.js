edc5a5ad7b94285e5d1c980ea2e3706cff715d4a
Vicavio <cuenta.vica@gmail.com>
2021-07-05 12:15:44 +0200

    Commit all files

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,18 +9,20 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hello, my name is',
+  name: 'Viorica',
+  subtitle: 'I am the Unknown Developer.',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'me-image.jpg',
+  paragraphOne: 'I am a beginner front-end developer.',
+  paragraphTwo:
+    'My journey in web development started a few months ago and I knew literally nothing about programming.',
+  paragraphThree:
+    'Now, I’ve improved a lot. Being a web developer has given me insight on how the internet as a whole works.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,30 +30,33 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'registration-form.jpg',
+    title: 'Form Registration',
+    info: 'Simple client side form validation. Check required, length, email and password match.',
+    info2:
+      'This form has to validate the details user is entering in each field and in case validation fails it has to highlight the invalid field and display the related message.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/Vicavio/Registration-form.git', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'todo-react.jpg',
+    title: 'To-Do App',
+    info: 'Build a Todo App with React ',
+    info2:
+      'The application will display the tasks, add new tasks, mark tasks as complete, and remove tasks',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/Vicavio/To-Do-App.git', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'random-jokes.jpg',
+    title: 'Random Jokes',
+    info: 'Create a web page that will display random jokes on a button click',
+    info2:
+      'An application that fetches a new  joke each time when you hit a button, and it displays it in the browser. Using Javascript, HTML/CSS and web api.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/Vicavio/Random-Jokes.git', // if no repo, the button will not show up
   },
 ];
 
@@ -68,27 +73,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      url: 'https://twitter.com/vicacrudu/status/509700000398712832?s=21',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/feed/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/Vicavio?tab=repositories',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
